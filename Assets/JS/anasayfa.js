93ab6819fb759a585889e68eabb224907f025c81
Yakupcan21 <yakupozdemir2003@hotmail.com>
2023-07-25 09:27:32 +0300

    Datas are pulled from database and used in website

--- Assets/JS/anasayfa.js
+++ Assets/JS/anasayfa.js
@@ -2,7 +2,7 @@
 function fetchFlightParameters() {
   const url = 'http://localhost:8080/seeAllFlightList';
 
-  return fetch(url)
+  fetch(url)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -11,33 +11,54 @@ function fetchFlightParameters() {
     })
     .then(data => {
       const flights = data;
+      console.log(flights)
 
-      const departure_airport = flights.map(flight => flight.departure_airport);
-      const arrival_airport = flights.map(flight => flight.arrival_airport);
-      const date = flights.map(flight => flight.date);
-      const time = flights.map(flight => flight.time);
-      const price = flights.map(flight => flight.price);
-      const flight_no = flights.map(flight => flight.flight_no);
+      let elFlightListContainer = document.querySelector(".flight-list-container");
 
-      console.log('departure_airport:', departure_airport);
-      console.log('arrival_airport:', arrival_airport);
-      console.log('date:', date);
-      console.log('time:', time);
-      console.log('price:', price);
-      console.log('flight_no:', flight_no);
+      let strFlights = "";
+      flights.forEach(flight => {
 
-      return {
-        departure_airport,
-        arrival_airport,
-        date,
-        time,
-        price,
-        flight_no,
-      };
+        strFlights += `<div class="flight">
+          <div class="departure">
+            <div>${flight.departure_airport}</div>
+          </div>
+          <div class="flight-no">
+            <div>${flight.flight_no}</div>
+          </div>
+          <div class="arrival">
+            <div>${flight.arrival_airport}</div>
+          </div>
+
+          <div class="time">
+            <div>${flight.date}</div>
+          </div>
+          <div class="price">
+            <div>${flight.price}</div>
+          </div>
+        </div>`;
+      });
+
+      elFlightListContainer.innerHTML = strFlights
+
+      // const departure_airport = flights.map(flight => flight.departure_airport);
+      // const arrival_airport = flights.map(flight => flight.arrival_airport);
+      // const date = flights.map(flight => flight.date);
+      // const time = flights.map(flight => flight.time);
+      // const price = flights.map(flight => flight.price);
+      // const flight_no = flights.map(flight => flight.flight_no);
+
+      // console.log('departure_airport:', departure_airport);
+      // console.log('arrival_airport:', arrival_airport);
+      // console.log('date:', date);
+      // console.log('time:', time);
+      // console.log('price:', price);
+      // console.log('flight_no:', flight_no);
+
+     
     })
     .catch(error => {
       console.error('Error fetching flight parameters:', error);
     });
 }
 
-  fetchFlightParameters();
+ fetchFlightParameters();
